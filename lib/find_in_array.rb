f70a8f2972eca5d18f13feba346e5b112faea329
congenial-course-2234 <jakelozano94@gmail.com>
2020-05-13 22:19:41 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,12 +1,12 @@
 def find_element_index(array, value_to_find)
   count = 0
-  if array[count] do
-    if array[count] == value_to_find
+  while array[count] do
+    if array[count] = value_to_find
       return count
     else
       count += 1
     end
-  else
-    return nil
   end
+  return nil
 end
+  
